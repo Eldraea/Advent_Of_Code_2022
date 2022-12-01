6d7a7931ff3a0101d25bb5b6c32dd4f2291d11e6
Kim Newton <kimberly.jacques@efrei.net>
2022-12-01 15:52:29 +0100

    Adding first part of the code

---
+++ Day_1_Calorie_Counting/Program.cs
@@ -0,0 +1,26 @@
+﻿string text = File.ReadAllText("input.txt");
+Console.WriteLine(GetTheMaximumTotalCaloriesCarried(text));
+
+int GetTheMaximumTotalCaloriesCarried(string text)
+{
+    string[] textSplitted = text.Split(new string[] { "\r\n\r\n" }, StringSplitOptions.None);
+
+    List<int> calories = new List<int>();
+    foreach (string line in textSplitted)
+    {
+        int[] lineSplitted = line.Split(new string[] { Environment.NewLine }, StringSplitOptions.None)
+             .Select(x => Int32.Parse(x)).ToArray();
+        calories.Add(lineSplitted.Sum());
+    }
+
+    return calories.Max();
+}
+
+
+
+
+
+
+
+
+
