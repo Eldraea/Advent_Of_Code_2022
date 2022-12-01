02626a6b6b2b45bcbaa8e065ce350d4fbbc5e9d9
Kim Newton <kimberly.jacques@efrei.net>
2022-12-01 17:03:56 +0100

    Refactoring

--- Day_1_Calorie_Counting/Program.cs
+++ Day_1_Calorie_Counting/Program.cs
@@ -1,8 +1,8 @@
 ﻿string text = File.ReadAllText("input.txt");
 List<int> calories = GetTheCaloriesCarriedByElves(text).OrderBy(x => x).ToList();
+
 Console.WriteLine($"The elf who is carring the most calories of food carries {PartOne(calories)} calories");
 Console.WriteLine($"The Total of calories carried by the three elves who are carring the most calories of food carry in total {PartTwo(calories)} calories");
-
 
 int PartOne(List<int> caloriesCarried)
     => calories.Last();
@@ -10,26 +10,15 @@ int PartOne(List<int> caloriesCarried)
 int PartTwo(List<int> caloriesCarried)
     => caloriesCarried.TakeLast(3).Sum();
 
-
 List<int> GetTheCaloriesCarriedByElves(string text)
 {
     string[] textSplitted = text.Split(new string[] { "\r\n\r\n" }, StringSplitOptions.None);
     List<int> calories = new List<int>();
 
-    foreach (string line in textSplitted)
+    foreach (string carriedCalories in textSplitted)
     {
-        int[] lineSplitted = line.Split(new string[] { Environment.NewLine }, StringSplitOptions.None)
-             .Select(x => Int32.Parse(x)).ToArray();
-        calories.Add(lineSplitted.Sum());
+        calories.Add(carriedCalories.Split(new string[] { Environment.NewLine }, StringSplitOptions.None)
+             .Select(x => Int32.Parse(x)).Sum());
     }
     return calories; ;
 }
-
-
-
-
-
-
-
-
-
