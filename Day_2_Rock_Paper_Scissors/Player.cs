77fcfd2b072090df6cf9bd0a8f0d232f25075c9b
Kim Newton <kimberly.jacques@efrei.net>
2022-12-02 08:39:34 +0100

    Refactoring

--- Day_2_Rock_Paper_Scissors/Player.cs
+++ Day_2_Rock_Paper_Scissors/Player.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Day_2_Rock_Paper_Scissors
+﻿namespace Day_2_Rock_Paper_Scissors
 {
     public abstract class Player
     {
