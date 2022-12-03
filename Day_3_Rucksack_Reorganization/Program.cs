08fc451a748ce81ae1ed29ffee7891433625d8fd
Kim Newton <kimberly.jacques@efrei.net>
2022-12-03 10:45:09 +0100

    Refactoring

--- Day_3_Rucksack_Reorganization/Program.cs
+++ Day_3_Rucksack_Reorganization/Program.cs
@@ -3,32 +3,24 @@
 string path = "input.txt";
 int[] letters = Enumerable.Range(97, 26).Concat(Enumerable.Range(65, 26)).ToArray(); ;
 
-Console.WriteLine($"The sum of the priotities of the item types is equal to {GetTheSumOfRucksacksCommonItemsForPartOne(path)}");
-Console.WriteLine($"The sum of the priotities of the item type that corresponds to the badges of each three-Elf group is equal to {GetTheSumOfRucksacksCommonItemsForPartTwo(path)}");
+Console.WriteLine($"The sum of the priotities of the item types is equal to {GetTheSumOfRucksacksCommonItems(path, 1)}");
+Console.WriteLine($"The sum of the priotities of the item type that corresponds to the badges of each three-Elf group is equal to {GetTheSumOfRucksacksCommonItems(path,2)}");
 
-int GetTheSumOfRucksacksCommonItemsForPartOne(string path)
+int GetTheSumOfRucksacksCommonItems(string path, int part)
 {
-    List<Rucksack> rucksacks = new();
-
+    List<int> rucksacksItems = new();
     using(StreamReader streamReader = new StreamReader(path))
     {
         string line;
-        while((line = streamReader.ReadLine())!=null)
-            rucksacks.Add(new Rucksack(line.Substring(0, line.Length/2), line.Substring(line.Length/2)));
+        if(part == 1)
+            while((line = streamReader.ReadLine())!=null)
+                rucksacksItems.Add(new Rucksack(line.Substring(0, line.Length/2), line.Substring(line.Length/2)).CommonItem);
+        else
+            while ((line = streamReader.ReadLine()) != null)
+                rucksacksItems.Add(new Rucksack(line, streamReader.ReadLine(), streamReader.ReadLine()).CommonItem);
     }
-    return rucksacks.Select(rucksack => Array.IndexOf(letters, rucksack.CommonItem) + 1).Sum();
+    return rucksacksItems.Select(rucksackItems => Array.IndexOf(letters, rucksackItems) + 1).Sum();
 }
 
-int GetTheSumOfRucksacksCommonItemsForPartTwo(string path)
-{
-    List<Group> rucksackGroups = new();
-    using (StreamReader streamReader = new StreamReader(path))
-    {
-        string line;
-        while ((line = streamReader.ReadLine()) != null)
-            rucksackGroups.Add(new Group(line, streamReader.ReadLine(),streamReader.ReadLine()));
-    }
-    return rucksackGroups.Select(rucksackGroup => Array.IndexOf(letters, rucksackGroup.CommonItem) +1).Sum();
-}
 
 
--- Day_3_Rucksack_Reorganization/Rucksack.cs
+++ Day_3_Rucksack_Reorganization/Rucksack.cs
@@ -3,14 +3,10 @@
     public class Rucksack
     {
         public int CommonItem { get; set; }
-        public string FirstCompartment { get; set; }
-        public string SecondCompartment { get; set; }
+        public Rucksack(string firstCompartment, string secondCompartment)
+            => CommonItem = firstCompartment.Where(x => secondCompartment.IndexOf(x) != -1).ToArray()[0];
+        public Rucksack(string firstRucksack, string secondRucksack, string thirdRucksack)
+            => CommonItem = firstRucksack.Where(x => secondRucksack.IndexOf(x) != -1 && thirdRucksack.IndexOf(x) != -1).ToArray()[0];
 
-        public Rucksack(string firstCompartment, string secondCompatment)
-        {
-            FirstCompartment = firstCompartment;
-            SecondCompartment = secondCompatment;
-            CommonItem = FirstCompartment.Where(x => SecondCompartment.IndexOf(x) != -1).ToArray()[0];
-        }
     }
 }
