fb6e795b228574581ab11d631ef311047cb6c5f1
Kim Newton <kimberly.jacques@efrei.net>
2022-12-03 10:50:11 +0100

    Refactoring

--- Day_3_Rucksack_Reorganization/Rucksack.cs
+++ Day_3_Rucksack_Reorganization/Rucksack.cs
@@ -2,11 +2,10 @@
 {
     public class Rucksack
     {
-        public int CommonItem { get; set; }
+        public int CommonItem { get; }
         public Rucksack(string firstCompartment, string secondCompartment)
             => CommonItem = firstCompartment.Where(x => secondCompartment.IndexOf(x) != -1).ToArray()[0];
         public Rucksack(string firstRucksack, string secondRucksack, string thirdRucksack)
             => CommonItem = firstRucksack.Where(x => secondRucksack.IndexOf(x) != -1 && thirdRucksack.IndexOf(x) != -1).ToArray()[0];
-
     }
 }
