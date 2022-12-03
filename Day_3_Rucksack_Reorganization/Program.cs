002ff6ccc592cf10bb26c6930b92db429bb822a7
Kim Newton <kimberly.jacques@efrei.net>
2022-12-03 11:04:45 +0100

    Refactoring

--- Day_3_Rucksack_Reorganization/Program.cs
+++ Day_3_Rucksack_Reorganization/Program.cs
@@ -1,6 +1,4 @@
-﻿using Day_3_Rucksack_Reorganization;
-
-string path = "input.txt";
+﻿string path = "input.txt";
 int[] letters = Enumerable.Range(97, 26).Concat(Enumerable.Range(65, 26)).ToArray(); ;
 
 Console.WriteLine($"The sum of the priotities of the item types is equal to {GetTheSumOfRucksacksCommonItems(path, 1)}");
@@ -14,12 +12,20 @@ int GetTheSumOfRucksacksCommonItems(string path, int part)
         string line;
         if(part == 1)
             while((line = streamReader.ReadLine())!=null)
-                rucksacksItems.Add(new Rucksack(line.Substring(0, line.Length/2), line.Substring(line.Length/2)).CommonItem);
+                rucksacksItems.Add(GetCommonItem(line.Substring(0, line.Length/2), line.Substring(line.Length/2)));
         else
             while ((line = streamReader.ReadLine()) != null)
-                rucksacksItems.Add(new Rucksack(line, streamReader.ReadLine(), streamReader.ReadLine()).CommonItem);
+                rucksacksItems.Add(GetCommonItem(line, streamReader.ReadLine(), streamReader.ReadLine()));
     }
     return rucksacksItems.Select(rucksackItems => Array.IndexOf(letters, rucksackItems) + 1).Sum();
+}
+
+int GetCommonItem(string firstRucksack, string secondRucksack, string? thirdRucksack = null)
+{
+    if (thirdRucksack is not null)
+        return firstRucksack.Where(x => secondRucksack.IndexOf(x) != -1 && thirdRucksack?.IndexOf(x) != -1).ToArray()[0];
+    else
+        return firstRucksack.Where(x => secondRucksack.IndexOf(x) != -1).ToArray()[0];
 }
 
 
