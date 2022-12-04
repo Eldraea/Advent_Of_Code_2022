8ed178191fa752e064542ff102ef166df68b33a8
Kim Newton <kimberly.jacques@efrei.net>
2022-12-04 12:49:32 +0100

    Adding Day 4

---
+++ Day_4_Camp_Cleanup/Program.cs
@@ -0,0 +1,54 @@
+﻿string path = "input.txt";
+
+
+PartOne(path);
+PartTwo(path);
+
+void PartOne(string path)
+{
+    int sum = 0;
+    using (StreamReader streamReader = new StreamReader(path))
+    {
+       
+        string line;
+        while ((line = streamReader.ReadLine()) is not null)
+        {
+            var lineSplitted = line.Split(',');
+            int firstNumber = int.Parse(lineSplitted[0].Substring(0, lineSplitted[0].IndexOf('-'))) - 1;
+            int secondNumber = int.Parse(lineSplitted[0].Substring(lineSplitted[0].IndexOf('-') + 1));
+            int thirdNumber = int.Parse(lineSplitted[1].Substring(0, lineSplitted[1].IndexOf('-'))) - 1;
+            int fourthNumber = int.Parse(lineSplitted[1].Substring(lineSplitted[1].IndexOf('-') + 1));
+            var firstTasks = Enumerable.Range(firstNumber, secondNumber - firstNumber).ToArray();
+            var secondTasks = Enumerable.Range(thirdNumber, fourthNumber - thirdNumber).ToArray();
+            var containSameTasks = firstTasks.Where(x => Array.IndexOf(secondTasks, x) != -1);
+            if (containSameTasks.Count() == firstTasks.Count() || containSameTasks.Count() == secondTasks.Count())
+                sum++;
+        }
+    }
+    Console.WriteLine(sum);
+
+}
+
+void PartTwo(string path)
+{
+    int sum = 0;
+    using (StreamReader streamReader = new StreamReader(path))
+    {
+        
+        string line;
+        while ((line = streamReader.ReadLine()) is not null)
+        {
+            var lineSplitted = line.Split(',');
+            int firstNumber = int.Parse(lineSplitted[0].Substring(0, lineSplitted[0].IndexOf('-'))) - 1;
+            int secondNumber = int.Parse(lineSplitted[0].Substring(lineSplitted[0].IndexOf('-') + 1));
+            int thirdNumber = int.Parse(lineSplitted[1].Substring(0, lineSplitted[1].IndexOf('-'))) - 1;
+            int fourthNumber = int.Parse(lineSplitted[1].Substring(lineSplitted[1].IndexOf('-') + 1));
+            var firstTasks = Enumerable.Range(firstNumber, secondNumber - firstNumber).ToArray();
+            var secondTasks = Enumerable.Range(thirdNumber, fourthNumber - thirdNumber).ToArray();
+            var containSameTasks = firstTasks.Where(x => Array.IndexOf(secondTasks, x) != -1);
+            if (containSameTasks.Count() !=0)
+                sum++;
+        } 
+    }
+    Console.WriteLine(sum);
+}
