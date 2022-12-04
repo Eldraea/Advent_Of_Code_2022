c2ecd094fa3ef4e0752485ff4863ce6d0097613c
Kim Newton <kimberly.jacques@efrei.net>
2022-12-04 13:02:54 +0100

    Refactoring

--- Day_4_Camp_Cleanup/Program.cs
+++ Day_4_Camp_Cleanup/Program.cs
@@ -16,7 +16,7 @@ int GetTheSumOfOverLappingPairs(string path, int part)
             int secondNumber = int.Parse(lineSplitted[0].Substring(lineSplitted[0].IndexOf('-') + 1));
             int thirdNumber = int.Parse(lineSplitted[1].Substring(0, lineSplitted[1].IndexOf('-'))) - 1;
             int fourthNumber = int.Parse(lineSplitted[1].Substring(lineSplitted[1].IndexOf('-') + 1));
-            var firstTasks = Enumerable.Range(firstNumber, secondNumber - firstNumber).ToArray();
+            var firstTasks = Enumerable.Range(firstNumber, secondNumber - firstNumber);
             var secondTasks = Enumerable.Range(thirdNumber, fourthNumber - thirdNumber).ToArray();
             var containSameTasks = firstTasks.Where(x => Array.IndexOf(secondTasks, x) != -1);
             if (part == 1 && (containSameTasks.Count() == firstTasks.Count() || containSameTasks.Count() == secondTasks.Count()))
