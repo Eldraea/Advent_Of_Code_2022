36a434871916dd483fb7357d9f261fa0951728c1
Kim Newton <kimberly.jacques@efrei.net>
2022-12-06 19:27:33 +0100

    Adding Day 6

---
+++ Day_6_Tuning_Trouble/Day_6.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_6_Tuning_Trouble
+{
+    public class Day_6
+    {
+        public int Result { get; }
+        public Day_6(string input, int part)
+         => Result = GetResult(input, part);
+     
+        private int GetResult(string input, int part)
+        {
+                int marker = part == 1 ? 4 : 14;
+                for (var i = 0; i < input.Length; i++)
+                {
+                    var charactersBeforeMarker = input.Substring(i, marker).ToCharArray();
+                    if (charactersBeforeMarker.Length == charactersBeforeMarker.Distinct().Count())
+                        return i + marker;   
+                }
+
+                return -1;
+        } 
+        
+    }
+}
+
---
+++ Day_6_Tuning_Trouble/Program.cs
@@ -0,0 +1,8 @@
+﻿using Day_6_Tuning_Trouble;
+
+string path = "../../../input.txt";
+string text = File.ReadLines(path).First();
+
+Day_6 daySix = new(text, 2);
+Console.WriteLine(daySix.Result);
+
