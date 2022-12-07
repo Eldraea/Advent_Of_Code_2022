1f317675714882fc1b3c5a27e937f34b49c69b5a
Kim Newton <kimberly.jacques@efrei.net>
2022-12-07 17:18:21 +0100

    Adding Day 7

---
+++ Day_7_No_Space_Left_On_Device/FileInDirectory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_7_No_Space_Left_On_Device
+{
+    public class FileInDirectory
+    {
+        public string Name { get;  }
+        public int Size { get;  }
+        public FileInDirectory(string[] input)
+        {
+            Name = input[1];
+            Size = int.Parse(input[0]);
+        }
+    }
+}
---
+++ Day_7_No_Space_Left_On_Device/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+
+string path = "../../../input.txt";
+string text = File.ReadAllText(path);
+
+Console.WriteLine(GetResult(text, 1));
+Console.WriteLine(GetResult(text, 2));
+
+int GetResult(string input, int part)
+{
+    var currentDirectory = new Stack<string>();
+    var directorySizes = new Dictionary<string, int>();
+    foreach (var line in input.Split(Environment.NewLine, StringSplitOptions.None))
+    {
+        if (line == "$ cd ..")
+            currentDirectory.Pop();
+        else if (line.StartsWith("$ cd"))
+            currentDirectory.Push(string.Join("", currentDirectory) + line.Split(" ").Last());
+        else if (Regex.Match(line, @"\d+").Success)
+        {
+            int size;
+            int.TryParse(line.Split(" ").First(), out size);
+            foreach (var directory in currentDirectory)
+                directorySizes[directory] = directorySizes.GetValueOrDefault(directory) + size; 
+        }
+    }
+    if (part == 1)
+        return directorySizes.Values.Where(x => x <= 100000).Sum();
+    else
+    {
+        var totalDiskSpace = 70000000 - directorySizes.Values.Max();
+        return directorySizes.Values.Where(x => totalDiskSpace + x >= 30000000).Min();
+    }
+}
+
+
